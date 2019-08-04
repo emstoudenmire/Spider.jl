c79e2708ec6f2a20a4255076ec7e3e13adc41629
Miles Stoudenmire <miles.stoudenmire@gmail.com>
2019-08-04 11:04:37 -0400

    Removed some unused code

--- src/util.jl
+++ src/util.jl
@@ -1,36 +1,10 @@
 export ArgDict
 
-const ArgDict = Dict{String,Any}
-
 function pause() 
   a = readline()
   (length(a) == 0) && return
   (a[1]=='q') && exit(0)
 end
-
-function getArg(args::ArgDict,
-                key::String,
-                default=Nothing)
-  if haskey(args,key)
-    return args[key]
-  end
-  if default == Nothing
-    throw(ArgumentError("key \"$key\" not found in argument dictionary"))
-  end
-  return default
-end
-
-#function getArg(args::ArgDict,
-#                key::String,
-#                default=Nothing)
-#  if haskey(args,key)
-#    return args[key]
-#  end
-#  if default == Nothing
-#    throw(ArgumentError("key \"$key\" not found in argument dictionary"))
-#  end
-#  return default
-#end
 
 function getArg(args,
                 key::Symbol,
